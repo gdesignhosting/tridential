8529671e5a0210dfc09caf2c61a70cc844eb0603
GDesign <info@gittinsdesign.com>
2019-10-27 20:28:15 +1000

    Initial commit

---
+++ finance/post.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Tridential Pty Ltd -  The Australian Finance Strategists</title>
+<script language="JavaScript1.2">
+function alert(){}
+</script>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<meta name="robots" content="noindex, nofollow" /></head>
+
+<body>
+
+<script language="php">
+$email = $HTTP_POST_VARS[email];
+$mailto = "tavis@tridential.com.au";
+$mailsubj = "Newsletter From Website";
+$mailhead = "From: Tridential.com";
+reset ($HTTP_POST_VARS);
+$mailbody = "Values submitted from web site form:\n";
+while (list ($key, $val) = each ($HTTP_POST_VARS)) { $mailbody .= "$key : $val\n"; }
+if (!eregi("\n",$HTTP_POST_VARS[email])) { mail($mailto, $mailsubj, $mailbody, $mailhead); }
+</script>
+<div id="container">    
+
+<div id="top">
+<div id="top-left"><img src="images/phone.jpg" alt="Tridential - Finance Strategists - Phone Number" /></div>
+<div id="top-right"><a href="http://www.tridential.com"><img src="images/home.jpg" border="0" align="top" alt="Tridential - Finance Strategists - Home Page" title="Home Page" /></a> <a href="http://www.tridential.com.au">Home</a> <a href="mailto:tavis@tridential.com.au"><img src="images/email.jpg" border="0" align="top" alt="Tridential - Finance Strategists - Contacts" title="Contacts" /></a> <a href="mailto:tavis@tridential.com.au">Contacts</a></div>
+</div>
+<div id="nav">
+<div id="nav-left"></div>
+<div id="nav-right"><div id="menu"><ul><li><a href="index.html">Home</a></li> <li><a href="about.html">Why Choose Us</a></li> 
+<li><a href="how-to-apply.html">How to Apply</a></li>
+<li><a href="services.html">Services</a></li> 
+<li><a href="associates.html">Associates</a></li> 
+<li><a href="contacts.html">Contacts</a></li>
+</ul></div>
+</div></div>
+<div id="newsletter">
+<div id="newsletterform"><form action="post.php" name="Newsletter" method="post">Name:<br /> <input name="Name" size="18" type="text" /><br /><br /> Email: <br /><input name="Email" size="18" type="text" /> <br /> <br /><input src="images/subscribe-button.jpg" name="Subscribe" value="Subscribe" type="image" /> </form></div>
+
+</div>
+<div id="banner"></div>
+
+
+<div id="content">
+
+
+
+<h1>Newsletter Subscribtion </h1>
+
+
+
+<p>Thank you for subscribing to Tridential's monthly newsletter. Check your emails regularly for each months copy, which includes important information and helpful hints. </p>
+</div>
+<div id="feature-image"><img src="images/feature.jpg" alt="Tridential - The Australian Finance Strategists" /></div>
+
+
+
+
+
+
+
+
+
+
+
+<div id="footer"> <div id="footerleft">Copyright 2008, Tridential Pty Ltd</div> <div id="footerright">Web Page Design by <a href="" target="_blank"> </a></div> 
+</div>
+</div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-3356145-7");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script></body>
+</html>
